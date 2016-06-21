45743a20e3133f992ac75317fd277b179fddca00
KathyGremillet <gremillet.kathy@gmail.com>
2016-06-21 14:45:46 +0200

    #2 creation du modèle

---
+++ app/MyBooks/MyBooks/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  MyBooks
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 KotKot. All rights reserved.
+//
+
+import Foundation
+
+
+class Book {
+    
+    var title : String = "Title"
+    var author : String = "Author"
+    var description : String = "Description"
+    
+}
+
---
+++ app/MyBooks/MyBooks/BookStore.swift
@@ -0,0 +1,14 @@
+//
+//  BookStore.swift
+//  MyBooks
+//
+//  Created by Mastere 1 IT on 21/06/2016.
+//  Copyright © 2016 KotKot. All rights reserved.
+//
+
+import Foundation
+
+
+class BookStore {
+    
+}
