f569d7770d287ee954cdf798a039c129a7d4e04b
KathyGremillet <gremillet.kathy@gmail.com>
2016-06-21 17:01:48 +0200

    closes #3 Affichage de la liste des livres avec lien vers page détails

--- app/MyBooks/MyBooks/BookStore.swift
+++ app/MyBooks/MyBooks/BookStore.swift
@@ -11,4 +11,20 @@ import Foundation
 
 class BookStore {
     
+    var books : [Book] = []
+    
+    init() {
+        let book1 = Book()
+        book1.title = "Game of Thrones - Tome 1"
+        book1.author = "Georges RR Martin"
+        book1.description = "This is the first tome of the great Game of Thrones's series"
+        books.append(book1)
+        
+        let book2 = Book()
+        book2.title = "Game of Thrones - Tome 2"
+        book2.author = "Georges RR Martin"
+        book2.description = "This is the second tome of the great Game of Thrones's series"
+        books.append(book2)
+    }
+    
 }
